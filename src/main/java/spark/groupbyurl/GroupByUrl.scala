c24d99543ff1f70ede63d8b5c9b02608eacde753
wensheng.yang <yws0328@126.com>
2018-07-26 22:19:54 +0800

    spark

--- src/main/java/spark/groupbyurl/GroupByUrl.scala
+++ src/main/java/spark/groupbyurl/GroupByUrl.scala
@@ -7,6 +7,8 @@ object GroupByUrl {
   def main(args: Array[String]): Unit = {
     val config=new SparkConf().setAppName("groupbyurl").setMaster("local[2]")
     val sc=new SparkContext(config)
-    sc.textFile("hdfs://mini01:9000/spark/itcast").map(x=>(x.split("/t"))).map(arr=>(arr(1),1))
+    sc.textFile("hdfs://mini01:9000/spark/itcast").map(x=>(x.split("/t"))).map(arr=>(arr(1),1)).reduceByKey(_+_)
+      .sortBy(_._2,false).saveAsHadoopFile("hdfs://mini01:9000/spark/out1")
+    sc.stop()
   }
 }
