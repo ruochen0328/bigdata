63691170ff839aa31c4e19dc8e4270a810a017df
wensheng.yang <yws0328@126.com>
2018-07-26 22:15:58 +0800

    spark

---
+++ src/main/java/spark/groupbyurl/GroupByUrl.scala
@@ -0,0 +1,12 @@
+package spark.groupbyurl
+
+import org.apache.hadoop.hdfs.DFSClient.Conf
+import org.apache.spark.{SparkConf, SparkContext}
+
+object GroupByUrl {
+  def main(args: Array[String]): Unit = {
+    val config=new SparkConf().setAppName("groupbyurl").setMaster("local[2]")
+    val sc=new SparkContext(config)
+    sc.textFile("hdfs://mini01:9000/spark/itcast").map(x=>(x.split("/t"))).map(arr=>(arr(1),1))
+  }
+}
